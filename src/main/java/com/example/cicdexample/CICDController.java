e02b4009ba1d4cfdbb29d50b5d95eef4f404b7e1
tranphuc269 <phuc260900@gmail.com>
2022-11-15 09:12:37 +0700

    first commit

---
+++ src/main/java/com/example/cicdexample/CICDController.java
@@ -0,0 +1,12 @@
+package com.example.cicdexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CICDController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return  "Hello world";
+    }
+}
---
+++ src/main/java/com/example/cicdexample/CicdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdExampleApplication.class, args);
+    }
+
+}
